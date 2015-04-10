fe64b180678e0b086b5b69aca8e2d35dfe1fca50
Jeremy Schlatter <jeremy.schlatter@gmail.com>
2015-04-10 00:03:40 -0700

    the /compile endpoint works

--- hello/hello.go
+++ hello/hello.go
@@ -3,13 +3,20 @@ package hello
 import (
 	"bytes"
 	"fmt"
+	"go/ast"
 	"go/build"
+	"go/parser"
+	"go/token"
 	"io"
 	"io/ioutil"
 	"net/http"
+	"os"
+	"path/filepath"
 
 	"appengine"
 
+	gbuild "github.com/gopherjs/gopherjs/build"
+	"github.com/gopherjs/gopherjs/compiler"
 	"github.com/mailgun/godebug/gen"
 )
 
@@ -31,18 +38,18 @@ func compile(w http.ResponseWriter, r *http.Request) {
 	}
 	progBytes, err = instrumentWithGodebug(c, progBytes)
 	if err != nil {
+		c.Errorf("Failed to instrument Go code: %v", err)
 		http.Error(w, "Invalid Go program", http.StatusBadRequest)
 		return
 	}
-	w.Write(progBytes)
-	c.Infof("great success")
-	return
-	progBytes, err = compileToJS(progBytes)
+	progBytes, err = compileToJS(c, progBytes)
 	if err != nil {
+		c.Errorf("Failed to compile to javascript: %v", err)
 		http.Error(w, "Compilation to javascript failed", http.StatusInternalServerError)
 		return
 	}
 	w.Write(progBytes)
+	c.Infof("great success")
 }
 
 func instrumentWithGodebug(c appengine.Context, b []byte) ([]byte, error) {
@@ -68,8 +75,53 @@ func instrumentWithGodebug(c appengine.Context, b []byte) ([]byte, error) {
 	return out.Bytes(), nil
 }
 
-func compileToJS(b []byte) ([]byte, error) {
-	return b, nil
+func compileToJS(c appengine.Context, b []byte) ([]byte, error) {
+	var err error
+	pkg := &gbuild.PackageData{
+		Package: &build.Package{
+			Name:       "main",
+			ImportPath: "main",
+			GoFiles:    []string{"main.go"},
+		},
+	}
+
+	s := gbuild.NewSession(&gbuild.Options{Minify: true})
+	s.ImportContext.Import = func(path string) (*compiler.Archive, error) {
+		for _, dir := range []string{build.Default.GOROOT, build.Default.GOPATH} {
+			filename := filepath.Join(dir, "pkg", build.Default.GOOS+"_js_min", path) + ".a"
+			f, err := os.Open(filename)
+			if err != nil {
+				continue
+			}
+			defer f.Close()
+			a, err := compiler.ReadArchive(filepath.Join(dir, path)+".a", path, f, s.ImportContext.Packages)
+			if err != nil {
+				c.Errorf("for package %q, ReadArchive gave: %v", path, err)
+			}
+			return a, err
+		}
+		return nil, fmt.Errorf("failed to find archive for package %q", path)
+	}
+
+	fs := token.NewFileSet()
+	f, err := parser.ParseFile(fs, "main.go", b, 0)
+	if err != nil {
+		return nil, err
+	}
+
+	pkg.Archive, err = compiler.Compile("main", []*ast.File{f}, fs, s.ImportContext, true)
+	if err != nil {
+		return nil, err
+	}
+
+	deps, err := compiler.ImportDependencies(pkg.Archive, s.ImportContext.Import)
+	if err != nil {
+		return nil, err
+	}
+
+	var out bytes.Buffer
+	err = compiler.WriteProgramCode(deps, &compiler.SourceMapFilter{Writer: &out})
+	return out.Bytes(), err
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
